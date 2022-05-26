ab1b88abd643e4ae695316621b887b7223f1dbce
vnexpressn <102777079+vnexpressn@users.noreply.github.com>
2022-05-26 14:39:46 +0700

    Update ms.js

--- ms.js
+++ ms.js
@@ -1,1 +1,1 @@
-document.getElementById("demo").innerHTML = "Hello World";
+document.getElementById("demo").innerHTML = "Nữ hoàng nội y vừa kết hôn với Nhật Tinh Anh ANM1602; Cô cho biết yêu đắm đuối anh chàng và muốn sinh 10 đứa con cho anh-Theo báo lá cải";
